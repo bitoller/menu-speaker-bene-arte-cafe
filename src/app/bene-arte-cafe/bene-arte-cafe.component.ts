a9a1e5d627bb885538ad9f615ceb0cb2e8271c1c
bitoller <bi_toller@hotmail.com>
2023-08-10 17:01:45 -0300

    refactor: key events

--- src/app/bene-arte-cafe/bene-arte-cafe.component.ts
+++ src/app/bene-arte-cafe/bene-arte-cafe.component.ts
@@ -1,14 +1,12 @@
 import { Component, HostListener, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { Router, RouterModule } from '@angular/router';
 import { AudioService } from '../_shared/services/audio.service';
-import { MatDividerModule } from '@angular/material/divider';
+import { ActionButtonsComponent } from '../_shared/components/action-buttons/action-buttons.component';
 
 @Component({
   selector: 'app-bene-arte-cafe',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, RouterModule, MatDividerModule],
+  imports: [CommonModule, ActionButtonsComponent],
   templateUrl: './bene-arte-cafe.component.html',
   styleUrls: ['./bene-arte-cafe.component.scss'],
 })
@@ -20,41 +18,12 @@ export class BeneArteCafeComponent {
       case '0':
         this.playAudio();
         break;
-      case '1':
-        this.callWaiter();
-        break;
-      case '4':
-        this.previousScreen();
-        break;
-      case '5':
-        this.nextScreen();
-        break;
       default:
         break;
     }
   }
 
-  constructor(private _router: Router) {}
-
   playAudio() {
     this.audioService.play('./assets/bene/audio-bene.mp3');
-  }
-
-  nextScreen() {
-    this.audioService.next();
-    setTimeout(() => {
-      this._router.navigateByUrl('/menu');
-    }, 15);
-  }
-
-  previousScreen() {
-    this.audioService.back();
-    setTimeout(() => {
-      this._router.navigateByUrl('/instructions');
-    }, 15);
-  }
-
-  callWaiter() {
-    this.audioService.waiter();
   }
 }
--- src/app/instructions/instructions.component.ts
+++ src/app/instructions/instructions.component.ts
@@ -1,21 +1,13 @@
 import { Component, HostListener, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
-import { Router, RouterModule } from '@angular/router';
 import { AudioService } from '../_shared/services/audio.service';
-import { MatDividerModule } from '@angular/material/divider';
+import { ActionButtonsComponent } from '../_shared/components/action-buttons/action-buttons.component';
 
 @Component({
   selector: 'app-instructions',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatListModule,
-    MatButtonModule,
-    RouterModule,
-    MatDividerModule,
-  ],
+  imports: [CommonModule, MatListModule, ActionButtonsComponent],
   templateUrl: './instructions.component.html',
   styleUrls: ['./instructions.component.scss'],
 })
@@ -27,41 +19,12 @@ export class InstructionsComponent {
       case '0':
         this.playAudio();
         break;
-      case '1':
-        this.callWaiter();
-        break;
-      case '4':
-        this.previousScreen();
-        break;
-      case '5':
-        this.nextScreen();
-        break;
       default:
         break;
     }
   }
 
-  constructor(private _router: Router) {}
-
   playAudio() {
     this.audioService.play('./assets/instructions/audio-instructions.mp3');
-  }
-
-  nextScreen() {
-    this.audioService.next();
-    setTimeout(() => {
-      this._router.navigateByUrl('/bene-arte-cafe');
-    }, 15);
-  }
-
-  previousScreen() {
-    this.audioService.back();
-    setTimeout(() => {
-      this._router.navigateByUrl('/home');
-    }, 15);
-  }
-
-  callWaiter() {
-    this.audioService.waiter();
   }
 }
